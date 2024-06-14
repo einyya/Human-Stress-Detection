12ce95b4b8e0d849cb570e2391b71ff66317fb6d
einyya <einyya@post.bgu.ac.il>
2024-06-14 16:08:07 +0300

    merge detaframe

--- main.py
+++ main.py
@@ -4,6 +4,38 @@ import io
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    def concatDf(df_filtered, Log_df):
+        for idx, row in corsi_filtered.iterrows():
+            # Find the insertion point
+            insert_idx = Log_df[Log_df['Time'] > row['Time']].index.min()
+
+            # If no row found with higher time, append at the end
+            if pd.isna(insert_idx):
+                insert_idx = len(Log_df)
+
+            # Create a new row to insert
+            if df_filtered=='scales_df':
+                new_row = {
+                    'ID': row['ID'],
+                    'Timestamp': row['Timestamp'],
+                    'Task': row['Scale'],
+                    'Status': 'Value',
+                    'Time': row['Time']
+                }
+            if df_filtered=='corsi_df':
+                new_row = {
+                    'ID': row['subnum'],
+                    'Timestamp': row['Timestamp'],
+                    'Task': row['type'],
+                    'Status': 'INSERTED',
+                    'Time': row['Time']
+                }
+
+            # Insert the new row into Log_df
+            Log_df = pd.concat(
+                [Log_df.iloc[:insert_idx], pd.DataFrame([new_row]), Log_df.iloc[insert_idx:]]).reset_index(
+                drop=True)
+
     def openfile(ID,dataframe_path_log):
         with open(dataframe_path_log, 'r') as logfile:
             # Initialize an empty list to hold rows of data
@@ -48,10 +80,28 @@ if __name__ == '__main__':
     scales_path = fr'{data_path_battery}\{task}\data\{ID}\physioscales-{ID}.csv'
 
     Log_df=openfile(ID,dataframe_path_log)
+
+    scales_df = pd.read_csv(scales_path, header=None)
+    scales_df.columns = ['ID', 'Timestamp', 'Value']
     corsi_df=Utilities.load_dataframe(corsi_path)
     pasat_df=Utilities.load_dataframe(pasat_path)
     twocoladd_df=Utilities.load_dataframe(twocoladd_path)
-    scales_df=Utilities.load_dataframe(scales_path)
+
+
+    corsi_df['Timestamp'] = pd.to_datetime(corsi_df['Timestamp'])
+    corsi_df['Time'] = corsi_df['Timestamp'].dt.time
+    scales_df['Timestamp'] = pd.to_datetime(scales_df['Timestamp']).dt.time
+
+    corsi_filtered = corsi_df[corsi_df['allcorr'] == 0]
+
+    # Convert Log_df 'Time' column to datetime.time type
+    Log_df['Time'] = pd.to_datetime(Log_df['Timestamp']).dt.time
+
+    # Insert rows from corsi_filtered into Log_df
+    concatDf(corsi_filtered,Log_df)
+    concatDf(scales_df,Log_df)
+    concatDf(pasat_df,Log_df)
+    concatDf(twocoladd_df,Log_df)
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
